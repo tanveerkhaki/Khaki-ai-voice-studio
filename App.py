e0ee7911fe1af58287a73b720862dba73eed6c48
Tanveer khaki <tanveerkhaki21@gmail.com>
2026-05-02 23:29:54 +0500

    Update App.py

--- App.py
+++ App.py
@@ -9,7 +9,7 @@ st.set_page_config(
     page_icon="🎙️",
     layout="wide"
 )
-
+<meta name="google-site-verification" content="XMevMtwIoHVhCraYTMo_1miegWUMRu_ISGQGoT6qQKQ" />
 # --- 2. Professional Dark Theme Styling ---
 st.markdown("""
     <style>
